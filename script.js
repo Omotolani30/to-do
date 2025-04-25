8d015053af1c3f7a98a134e207f7bf0ddde8e4ef
Tolani <omotolaniafolayan85@gmail.com>
2025-04-25 18:48:09 +0100

    edit pop-up added

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-var todo = document.getElementById("todo");
-var showTodo = document.getElementById("showTodo");
-var errorMsg = document.getElementById("errorMsg");
-var allTodos = JSON.parse(localStorage.getItem("todos")) || []
+let todo = document.getElementById("todo");
+let showTodo = document.getElementById("showTodo");
+let errorMsg = document.getElementById("errorMsg");
+let allTodos = JSON.parse(localStorage.getItem("todos")) || [];
+let currentEditIndex;
+let modal = document.getElementById("modal");
+let close = document.getElementsByClassName("close")[0];
+let editInput = document.getElementById("editInput");
 
 
 function checkInput(event) {
     let key = event.key;
     if (key === "Enter") {
         addTodo()
+    }
+}
+
+function searchTodo() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let filteredTodos = allTodos.filter(todo => todo.toLowerCase().includes(search));
+    showTodo.innerHTML = '';
+
+    for(index=0; index < filteredTodos.length; index++) {
+        const elements = filteredTodos[index];
+
+        showTodo.innerHTML += `
+        <li class="text-white bg-[#3674B5] rounded-lg py-4 px-10 my-3 flex items-center justify-between">
+            <p>${elements}</p>
+            <div class="flex gap-8">
+                <button onclick='editTodo(${index})'>
+                    <img src="assets/edit.svg" class="opacity-60">
+                </button>
+                <button onclick='delTodo(${index})'>
+                    <img src="assets/icons8.svg" class="opacity-60">
+                </button>
+            </div>
+        </li>
+        `
+    }
+    if (filteredTodos.length === 0) {
+        showTodo.innerHTML = `<p class="text-white text-center">No results found</p>`
     }
 }
 
@@ -25,15 +56,27 @@ function addTodo() {
 }
 
 function editTodo(index) {
-    let edit = prompt("Kindly fill in your edit", allTodos[index])
-    if (edit) {
-        allTodos[index] = edit;
-        displayTodo();
+    currentEditIndex = index;
+
+    modal.style.display = 'block'
+    close.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function saveTodo() {
+    if (editInput.value) {
+        allTodos[currentEditIndex] = editInput.value;
         updateLocalStorage();
-    }
-    else {
+    } 
         displayTodo();
-    }
+        modal.style.display = "none";
 }
 
 function delTodo(index){
@@ -52,8 +95,8 @@ function displayTodo() {
     for(index=0; index < allTodos.length; index++) {
         const elements = allTodos[index];
 
-        showTodo.innerHTML += `
-        <li class="text-white bg-blue-600 rounded-lg py-4 px-10 my-3 flex items-center justify-between">
+        showTodo.innerHTML +=`
+        <li class="text-white bg-[#3674B5] hover:bg-blue-400 rounded-lg py-3 px-10 my-3 flex items-center justify-between">
             <p>${elements}</p>
             <div class="flex gap-8">
                 <button onclick='editTodo(${index})'>
